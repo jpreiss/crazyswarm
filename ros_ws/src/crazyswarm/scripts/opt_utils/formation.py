43d0880b709528fb46aaefc0a80888f685470412
jpreiss <jamesalanpreiss@gmail.com>
2021-01-26 13:28:18 -0800

    python2 compat, conda_env, interruptible loops

--- ros_ws/src/crazyswarm/scripts/opt_utils/formation.py
+++ ros_ws/src/crazyswarm/scripts/opt_utils/formation.py
@@ -210,12 +210,12 @@ def Hc(drone_pos, fov_radius, focal_length=0.04, sigma=0.3, R=30, kappa=0.5):
     b2 = R
     c2 = 2 * (sigma ** 2)
 
-    # scheme = quadpy.disk.lether(6)
-    scheme = quadpy.s2.get_good_scheme(6)
-    val = scheme.integrate(lambda p: (fpers(p[0], a1, b1, c1) *
-                                      fres(p[0], a2, b2, c2)),
-                           [x, y], r
-                           )
+    val = quadpy.disk.integrate(
+        lambda p: (fpers(p[0], a1, b1, c1) * fres(p[0], a2, b2, c2)),
+        [x, y],
+        r,
+        quadpy.disk.Lether(6),
+    )
     return val
 
 
@@ -256,17 +256,18 @@ def Ho(drone1_pos, drone2_pos, fov1_radius, fov2_radius, focal_length=0.04, sigm
     b4 = R
     c4 = 2 * (sigma ** 2)
 
-    # scheme = quadpy.disk.lether(6)
-    scheme = quadpy.s2.get_good_scheme(6)
-    val1 = scheme.integrate(lambda x: (fpers(x[0], a1, b1, c1) *
-                                       fres(x[0], a2, b2, c2)),
-                           [mid_x, mid_y], overlap
-                           )
-
-    val2 = scheme.integrate(lambda x: (fpers(x[0], a3, b3, c3) *
-                                       fres(x[0], a4, b4, c4)),
-                            [mid_x, mid_y], overlap
-                            )
+    val1 = quadpy.disk.integrate(
+        lambda x: (fpers(x[0], a1, b1, c1) * fres(x[0], a2, b2, c2)),
+        [mid_x, mid_y],
+        overlap,
+        quadpy.disk.Lether(6),
+    )
+    val2 = quadpy.disk.integrate(
+        lambda x: (fpers(x[0], a3, b3, c3) * fres(x[0], a4, b4, c4)),
+        [mid_x, mid_y],
+        overlap,
+        quadpy.disk.Lether(6),
+    )
     return min(val1, val2)
 
 
