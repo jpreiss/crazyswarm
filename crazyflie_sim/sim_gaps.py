1085fed0c0b7b893187f0f9c4a6f0de2492715f5
jpreiss <jamesalanpreiss@gmail.com>
2024-03-15 18:59:03 -0700

    dead code removal

--- crazyflie_sim/sim_gaps.py
+++ crazyflie_sim/sim_gaps.py
@@ -9,51 +9,6 @@ from crazyflie_sim.backend.np import Quadrotor
 from crazyflie_sim.sim_data_types import State
 
 
-def a2v(a):
-    v = cffirmware.vec3_s()
-    v.x = a[0]
-    v.y = a[1]
-    v.z = a[2]
-    return v
-
-
-def a2att(a):
-    att = cffirmware.attitude_t()
-    att.roll = a[0]
-    att.pitch = a[1]
-    att.yaw = a[2]
-    return att
-
-
-def v2a(v):
-    return np.array([v.x, v.y, v.z], dtype=np.float32)
-
-
-def zero_inputs():
-    setpoint = cffirmware.setpoint_t()
-    setpoint.mode.x = cffirmware.modeAbs
-    setpoint.mode.y = cffirmware.modeAbs
-    setpoint.mode.z = cffirmware.modeAbs
-    setpoint.mode.yaw = cffirmware.modeAbs
-    setpoint.position = a2v(np.zeros(3))
-    setpoint.yaw = 0
-    setpoint.velocity = a2v(np.zeros(3))
-    setpoint.attitudeRate = a2att(np.zeros(3))
-    setpoint.attitude = a2att(np.zeros(3))
-
-    state = cffirmware.state_t()
-    state.position = a2v(np.zeros(3))
-    state.velocity = a2v(np.zeros(3))
-    state.attitude = a2att(np.zeros(3))
-
-    sensors = cffirmware.sensorData_t()
-    sensors.gyro.x = 0
-    sensors.gyro.y = 0
-    sensors.gyro.z = 0
-
-    return setpoint, state, sensors
-
-
 def rollout(sim: Quadrotor, cf: CrazyflieSIL):
     HZ = 1000
 
@@ -69,8 +24,6 @@ def rollout(sim: Quadrotor, cf: CrazyflieSIL):
     omega = 2 * np.pi / period
 
     T = int(3 * period * HZ) + 1
-
-    setpoint, _, _ = zero_inputs()
 
     state_log = []
     target_log = []
@@ -133,52 +86,6 @@ def main():
     for ax in axs:
         ax.legend()
     fig.savefig("gaps_cf.pdf")
-
-
-
-def rollout_circle(ctrl):
-    states = []
-
-    dt = 1.0 / cffirmware.ATTITUDE_RATE
-
-    setpoint, state, sensors = zero_inputs()
-
-    for i in range(10000):
-        state.position.x = 0.1
-
-
-    command = cffirmware.control_t()
-    command_gaps = cffirmware.control_t()
-
-
-def test_gaps_circle_tracking():
-
-    dt = 1.0 / cffirmware.ATTITUDE_RATE
-
-    ctrl = cffirmware.controllerMellinger_t()
-    ctrl_gaps = cffirmware.controllerMellinger_t()
-    for c in [ctrl, ctrl_gaps]:
-        cffirmware.controllerMellingerInit(c)
-    ctrl_gaps.gaps_enable = 1
-    ctrl_gaps.gaps_eta = 1e-3
-
-    # introduce some error
-    setpoint, state, sensors = zero_inputs()
-    state.position.x = 0.1
-    step = 0
-
-    command = cffirmware.control_t()
-    command_gaps = cffirmware.control_t()
-
-    for i in range(100):
-        cffirmware.controllerMellinger(
-            ctrl, command, setpoint, sensors, state, step)
-        cffirmware.controllerMellinger(
-            ctrl_gaps, command_gaps, setpoint, sensors, state, step)
-        if i == 0:
-            assert control_equal(command, command_gaps)
-        else:
-            assert not control_equal(command, command_gaps)
 
 
 if __name__ == "__main__":
