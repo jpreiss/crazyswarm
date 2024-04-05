f4d09f27202baef3847559dd279e86454d19648c
jpreiss <jamesalanpreiss@gmail.com>
2024-04-05 15:32:05 -0700

    plots for first preliminary report

--- crazyflie_sim/sim_gaps.py
+++ crazyflie_sim/sim_gaps.py
@@ -2,6 +2,7 @@ from copy import copy, deepcopy
 import itertools as it
 
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 import numpy as np
 
 import cffirmware
@@ -20,7 +21,22 @@ PARAM_ATTRS = [
 HZ = 500
 
 
-def rollout(sim: Quadrotor, cf: CrazyflieSIL):
+def plot_colorchanging(ax, x, y, *args, **kwargs):
+    # Create a set of line segments so that we can color them individually
+    # This creates the points as an N x 1 x 2 array so that we can stack points
+    # together easily to get the segments. The segments array for line collection
+    # needs to be (numlines) x (points per line) x 2 (for x and y)
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    lc = LineCollection(segments, *args, **kwargs)
+    # Set the values used for colormapping
+    lc.set_array(np.linspace(0, 1, len(x)))
+    ax.add_collection(lc)
+    return lc
+    #fig.colorbar(line, ax=axs[0])
+
+
+def rollout(sim: Quadrotor, cf: CrazyflieSIL, adapt: bool):
 
     ticks = 0
 
@@ -28,7 +44,7 @@ def rollout(sim: Quadrotor, cf: CrazyflieSIL):
     period = 6.0
     omega = 2 * np.pi / period
 
-    repeats = 8
+    repeats = 4 + 4 * adapt
     T = int(repeats * period * HZ) + 1
 
     rng = np.random.default_rng(0)
@@ -51,9 +67,10 @@ def rollout(sim: Quadrotor, cf: CrazyflieSIL):
     angvel = np.zeros(3)
 
     # introduce a constant "wind" pushing us out of the desired fig8-plane
-    wind_osc = np.cos(2 * (2 * np.pi / T) * np.arange(T) - 0.4 * np.pi)
-    wind_y = np.clip(5 * wind_osc, -1, 1) - 1
-    w[:, 1] += 0.05 * wind_y
+    if adapt:
+        wind_osc = np.cos(2 * (2 * np.pi / T) * np.arange(T) - 0.4 * np.pi)
+        wind_y = np.clip(5 * wind_osc, -1, 1) - 1
+        w[:, 1] += 0.05 * wind_y
     #plt.plot(w[:, 1])
     #plt.show()
 
@@ -69,10 +86,9 @@ def rollout(sim: Quadrotor, cf: CrazyflieSIL):
         acc[2] = -radius * 2 * (omega ** 2) * np.sin(2 * omega * tsec)
         state_log.append(deepcopy(sim.state))
         target_log.append(State(pos=pos, vel=vel))
-        # TODO: control cost!!!
+        assert cf.mellinger_control.gaps_Qv == 0.0
         cost_log.append(
             0.5 * cf.mellinger_control.gaps_Qx * norm2(pos - sim.state.pos)
-            + 0.5 * cf.mellinger_control.gaps_Qv * norm2(vel - sim.state.vel)
         )
         cf.setState(sim.state)
         cf.cmdFullState(pos, vel, acc, yaw, angvel)
@@ -101,7 +117,7 @@ def rollout(sim: Quadrotor, cf: CrazyflieSIL):
     return state_log, target_log, cost_log, param_log, action_log, y_log
 
 
-def main():
+def main(adapt: bool):
     cfs = [
         CrazyflieSIL("", np.zeros(3), "mellinger", lambda: 0)
         for _ in range(2)
@@ -111,6 +127,7 @@ def main():
         cf.mellinger_control.kd_omega_rp = 0
         cf.mellinger_control.mass = Quadrotor(State()).mass
         # because it's annoying to extract the "u"
+        cf.mellinger_control.gaps_Qv = 0
         cf.mellinger_control.gaps_R = 0
         cf.mellinger_control.i_range_xy *= 0.5
 
@@ -118,7 +135,7 @@ def main():
     cfs[1].mellinger_control.gaps_eta = 1e-2
 
     results = [
-        rollout(Quadrotor(State()), cf)
+        rollout(Quadrotor(State()), cf, adapt=adapt)
         for cf in cfs
     ]
 
@@ -133,13 +150,31 @@ def main():
     fig, ax = plt.subplots()
     ax.plot(target[:, 0], target[:, 2], color="black")
     ax.axis("equal")
+    ax.set(ylabel="gravity", xlabel="horizontal")
     fig.savefig("target.pdf")
 
 
     W = 8
-    H = 2
+    H = 1.5
     def subplots(n):
         return plt.subplots(n, 1, figsize=(W, H*n), constrained_layout=True)
+
+    prefix = "gaps_cf"
+    if adapt:
+        prefix += "_adapt"
+
+    fig_fig8, axs_fig8 = plt.subplots(1, 2, figsize=(8.5, 2.5), constrained_layout=True)
+    for ax, log, name in zip(axs_fig8, state_logs[:2], names[:2]):
+        pos = np.stack([s.pos for s in log])
+        ax.plot(target[:, 0], target[:, 2], label="target", color="gray", linewidth=1)
+        #ax.plot(pos[:, 0], pos[:, 2], label=name, color="black", linewidth=2)
+        cmap = "viridis"
+        line = plot_colorchanging(ax, pos[:, 0], pos[:, 2], label=name, cmap=cmap, linewidth=2)
+        ax.set(title=name, xlabel="horizontal (m)", ylabel="gravity (m)")
+        ax.axis("equal")
+    cbar = fig_fig8.colorbar(line)
+    cbar.ax.set_ylabel("time")
+    fig_fig8.savefig(f"{prefix}_fig8.pdf")
 
     fig_pos, axs_pos = subplots(2)
     fig_vel, axs_vel = subplots(2)
@@ -153,8 +188,8 @@ def main():
         ax.legend()
     for ax in axs_vel:
         ax.legend()
-    fig_pos.savefig("gaps_cf_pos.pdf")
-    fig_vel.savefig("gaps_cf_vel.pdf")
+    fig_pos.savefig(f"{prefix}_pos.pdf")
+    fig_vel.savefig(f"{prefix}_vel.pdf")
 
     fig_cost, axs_cost = subplots(3)
     ax_cost, ax_cum, ax_regret = axs_cost
@@ -167,7 +202,7 @@ def main():
     ax_regret.set(ylabel="cum. cost difference")
     for ax in axs_cost:
         ax.legend()
-    fig_cost.savefig("gaps_cf_cost.pdf")
+    fig_cost.savefig(f"{prefix}_cost.pdf")
 
     param_logs = np.stack(results[1][3])
     T, theta_dim = param_logs.shape
@@ -175,7 +210,7 @@ def main():
     for trace, ax, name in zip(param_logs.T, axs_param, PARAM_ATTRS):
         ax.plot(t, trace)
         ax.set_ylabel(name)
-    fig_param.savefig("gaps_cf_params.pdf")
+    fig_param.savefig(f"{prefix}_params.pdf")
 
     action_logs = [np.stack(r[4]) for r in results]
     T, ac_dim = action_logs[0].shape
@@ -185,14 +220,15 @@ def main():
             ax.plot(t, trace, label=name)
     for ax in axs_action:
         ax.legend()
-    fig_action.savefig("gaps_cf_actions.pdf")
+    fig_action.savefig(f"{prefix}_actions.pdf")
 
     y_log = np.stack(results[1][5])
     assert y_log[0].shape == (9, 6)
     fig_y, ax_y = subplots(1)
     maxes = [np.amax(y.flat) for y in y_log]
     ax_y.plot(t, maxes)
-    fig_y.savefig("gaps_cf_ymax.pdf")
+    fig_y.savefig(f"{prefix}_ymax.pdf")
 
 if __name__ == "__main__":
-    main()
+    main(adapt=False)
+    main(adapt=True)
