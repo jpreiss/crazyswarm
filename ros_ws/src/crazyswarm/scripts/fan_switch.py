78b4eb2aabac02180736f04cdd159cae0d5548cf
jpreiss <jamesalanpreiss@gmail.com>
2024-04-15 21:36:08 -0700

    ROS node piping fan switch to Arduino

---
+++ ros_ws/src/crazyswarm/scripts/fan_switch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import serial
+import time
+
+import rospy
+import std_msgs
+
+
+arduino = None
+
+
+def callback(msg):
+    global arduino
+    s = "1" if msg.data else "0"
+    arduino.write(bytes(s, "ascii"))
+
+
+def main():
+    global arduino
+    arduino = serial.Serial("/dev/ttyACM0",  baudrate=115200, timeout=1)
+    # turn off initially
+    arduino.write(bytes("0", "ascii"))
+    rospy.init_node("fan_switcher")
+    rospy.Subscriber("fan", std_msgs.msg.Bool, callback)
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
