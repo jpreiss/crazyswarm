87f2241cdeb38e518993d7a0f220036f6a7dc54a
jpreiss <jamesalanpreiss@gmail.com>
2024-04-30 19:57:26 -0700

    update benchmark for 3D

--- ros_ws/jacobian/gapsquad/benchmark.cpp
+++ ros_ws/jacobian/gapsquad/benchmark.cpp
@@ -1,15 +1,15 @@
-#include "planar.hpp"
+#include "gapsquad.hpp"
 
 int main()
 {
-	Vec ierr; Vec p; Vec v; FLOAT r; FLOAT w; // state
-	Vec p_d; Vec v_d; Vec a_d; FLOAT w_d; // target
+	Vec ierr; Vec p; Vec v; Mat R; Vec w; // state
+	Vec p_d; Vec v_d; Vec a_d; FLOAT y_d; Vec w_d; // target
 	FLOAT ki; FLOAT kp; FLOAT kv; FLOAT kr; FLOAT kw; // params
 
 	for (int i = 0; i < 10000000; ++i) {
 		ctrl(
-			ierr, p, v, r, w, // state
-			p_d, v_d, a_d, w_d, // target
+			ierr, p, v, R, w, // state
+			p_d, v_d, a_d, y_d, w_d, // target
 			ki, kp, kv, kr, kw // params
 		);
 	}
