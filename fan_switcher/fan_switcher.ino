5f7c5ea813a67d8b4890a81ce636099e501b4d39
jpreiss <jamesalanpreiss@gmail.com>
2024-05-24 22:47:11 -0700

    incorporate light controls into arduino + node

--- fan_switcher/fan_switcher.ino
+++ fan_switcher/fan_switcher.ino
@@ -1,32 +1,105 @@
+#include <Adafruit_NeoPixel.h>
+
+// if we go this long without hearing anything over serial, stop.
 int constexpr TIMEOUT = 1000;
 
+// neopixels config.
+int constexpr PIN_PIXELS = 4;
+int constexpr NUM_PIXELS = 115;
+
+// fan config.
+int constexpr PIN_FAN = 8;
+
+// state machine.
+enum State
+{
+  Idle,
+  Fan,
+  White,
+  Red,
+  Green,
+  Blue
+};
+
+Adafruit_NeoPixel pixels(NUM_PIXELS, PIN_PIXELS, NEO_GRB + NEO_KHZ800);
+
 static unsigned long last_data = 0;
+static State state = State::Idle;
+static uint32_t color = 0;
 
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(1);
+
+  pixels.begin();
+  // pixels have their own memory, but we want resetting the Arduino to reset them.
+  pixels.clear();
+  pixels.show();
+
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(PIN_FAN, OUTPUT);
+
+  Serial.println("Arduino is ready.");
 }
+
+// protocol:
+// if character 'f':
+//     next byte is '1' or '0' to change state
+// if character 'l':
+//     next 4 bytes are w, r, g, b values
+
 
 void  loop() {
   unsigned long m =  millis();
   while (!Serial.available()) {
     m = millis();
-    // TODO: Handle overflow.
+    // Timeout. TODO: Handle overflow.
     if (last_data > 0 && m - last_data > TIMEOUT) {
       digitalWrite(LED_BUILTIN, LOW);
       digitalWrite(8, LOW);
+      pixels.clear();
+      state = State::Idle;
     }
   }
   last_data = m;
   int x = Serial.read();
-  if (x == '1') {
-    digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(8, HIGH);
-  }
-  else if (x == '0') {
-    digitalWrite(LED_BUILTIN, LOW);
-    digitalWrite(8, LOW);
+  switch (state) {
+    case Idle:
+      if (x == 'f') {
+        // // Serial.println("fan");
+        state = Fan;
+      }
+      if (x == 'l') {
+        // Serial.println("light");
+        state = White;
+      }
+      // otherwise, do not understand
+      break;
+    case Fan:
+      if (x == '1') {
+        // Serial.println("on");
+        digitalWrite(LED_BUILTIN, HIGH);
+        digitalWrite(PIN_FAN, HIGH);
+      }
+      else if (x == '0') {
+        // Serial.println("off");
+        digitalWrite(LED_BUILTIN, LOW);
+        digitalWrite(PIN_FAN, LOW);
+      }
+      state = Idle;
+      break;
+    case White:
+    case Red:
+    case Green:
+      color = (color << 8) | x;
+      state = (int)state + 1;
+      break;
+    case Blue:
+      // Serial.println("b");
+      color = (color << 8) | x;
+      pixels.fill(color);
+      pixels.show();
+      state = State::Idle;
+      break;
   }
 }
--- ros_ws/src/crazyswarm/scripts/fan_switch.py
+++ ros_ws/src/crazyswarm/scripts/fan_switch.py
@@ -9,12 +9,22 @@ import std_msgs
 
 
 arduino = None
+CHR_L = ord("l")
+READY_MSG = bytes("Arduino is ready.", "ascii")
 
 
-def callback(msg):
+def fan_callback(msg):
     global arduino
-    s = "1" if msg.data else "0"
+    s = "f1" if msg.data else "f0"
     arduino.write(bytes(s, "ascii"))
+
+
+def light_callback(msg):
+    global arduino
+    rgb = [int(255 * c) & 0xFF for c in [msg.r, msg.g, msg.b]]
+    w = 0
+    b = bytes([CHR_L, w] + rgb)
+    arduino.write(b)
 
 
 def main():
@@ -24,11 +34,25 @@ def main():
         raise IOError("No Arduino serial device found.")
     dev = devs[0]
     print("using device " + dev)
-    arduino = serial.Serial(dev,  baudrate=115200, timeout=1)
-    # turn off initially
-    arduino.write(bytes("0", "ascii"))
+    arduino = serial.Serial(dev, baudrate=115200, timeout=1)
+
+    # wait for arduino to be ready
+    while True:
+        line = arduino.readline()
+        # because it sends weird line endings
+        if line.startswith(READY_MSG):
+            break
+    print("arduino is ready.")
+
+    # turn fan off initially
+    arduino.write(bytes("f0", "ascii"))
+    # turn light off initially
+    b = bytes([CHR_L, 0, 0, 0, 0])
+    arduino.write(b)
+
     rospy.init_node("fan_switcher")
-    rospy.Subscriber("fan", std_msgs.msg.Bool, callback)
+    rospy.Subscriber("fan", std_msgs.msg.Bool, fan_callback, queue_size=1)
+    rospy.Subscriber("light", std_msgs.msg.ColorRGBA, light_callback, queue_size=1)
     rospy.spin()
 
 
