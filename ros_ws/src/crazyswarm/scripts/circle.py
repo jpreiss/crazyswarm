ede0a85751919756020f68192c1379fa3063a40f
jpreiss <jamesalanpreiss@gmail.com>
2024-05-24 22:46:17 -0700

    circle code for hoop experiment (WIP)

--- ros_ws/src/crazyswarm/scripts/circle.py
+++ ros_ws/src/crazyswarm/scripts/circle.py
@@ -6,6 +6,8 @@ Flies a circle with constant body-frame roll/pitch and rotating yaw, like a
 fixed-wing, or a human piloting a rotorcraft from first-person view.
 """
 
+import itertools as it
+
 import numpy as np
 
 from pycrazyswarm import *
@@ -35,26 +37,45 @@ def circle(timeHelper, cf, z, radius=0.1, period=6, loops=1, rate=50):
         timeHelper.sleepForRate(rate)
 
 
-if __name__ == "__main__":
+PARAMS = [
+    p + s for p, s in it.product(["ki_", "kp_", "kv_", "kr_", "kw_"], ["xy", "z"])
+]
+
+def main():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     cf = swarm.allcfs.crazyflies[0]
 
+    # log space params!
+    full_params = ["gaps6DOF/" + p for p in PARAMS]
+    values = [cf.getParam(p) - float(np.log(2)) for p in full_params]
+    cf.setParams(dict(zip(full_params, values)))
+
+    if True:
+        params = {
+            "eta": 2e-2,
+            "optimizer": 0,  # OGD
+            "enable": 1,
+        }
+        params = {"gaps6DOF/" + k: v for k, v in params.items()}
+        cf.setParams(params)
+
+
+
     rate = 30.0
-    Z = 0.5
-    radius = 0.6
+    Z = 0.99
+    radius = 0.65
+
+    periods = [5, 4.5, 4, 3.5,  3, 2.75, 2.5, 2.25, 2]
+    periods = periods + periods[::-1]
 
     cf.takeoff(targetHeight=Z, duration=Z+1.0)
     timeHelper.sleep(Z+1.0)
-    cf.goTo(cf.initialPosition + [0, 0, Z], yaw=0, duration=1.0)
-    timeHelper.sleep(2.0)
+    cf.goTo(cf.initialPosition + [0, 0, Z], yaw=0, duration=2.0)
+    timeHelper.sleep(3.0)
 
-    # Uncomment inner calls for an aggressive flight!
-    circle(timeHelper, cf, Z, radius=radius, period=4)
-    #circle(timeHelper, cf, Z, radius=radius, period=2.5)
-    ##circle(timeHelper, cf, Z, radius=radius, period=2.0, loops=2)
-    #circle(timeHelper, cf, Z, radius=radius, period=2.5)
-    circle(timeHelper, cf, Z, radius=radius, period=4)
+    for period in periods:
+        circle(timeHelper, cf, Z, radius=radius, period=period)
 
     cf.notifySetpointsStop()
     timeHelper.sleep(0.01)
@@ -62,3 +83,7 @@ if __name__ == "__main__":
     timeHelper.sleep(2.0)
     cf.land(targetHeight=0.03, duration=Z+1.0)
     timeHelper.sleep(Z+1.0)
+
+
+if __name__ == "__main__":
+    main()
