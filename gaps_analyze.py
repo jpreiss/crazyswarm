18eac5d5fcf9bf9662f27cc3cf41ca75d130c84e
jpreiss <jamesalanpreiss@gmail.com>
2024-07-08 23:41:10 -0700

    remove original plot_costs

--- gaps_analyze.py
+++ gaps_analyze.py
@@ -187,47 +187,6 @@ def plot_fig8(dfs, style):
 
 def plot_costs(dfs: Sequence[pd.DataFrame], style):
 
-    # TODO: work on the layout of this figure + what should stay in final paper.
-    fig_cost, axs_cost = plt.subplots(4, 1, figsize=(8, 8), constrained_layout=True)
-    ax_cost, ax_err, ax_cum, ax_regret = axs_cost
-
-    # take downsampled means to smoot the plots a little.
-    maxtime = max(df[TIME].max() for df in dfs)
-    dfs_sampled = []
-    for df in dfs:
-        df["timedelta"] = pd.to_timedelta(df[TIME], unit="seconds")
-        dfi = df.set_index("timedelta")
-        dfr = dfi.resample("100ms").apply(agg)
-        # this used to be before resampling, but that was wrong!
-        dfr[COST_CUM] = (dfr["cost"] * dfr[TIME].diff()).cumsum()
-        dfs_sampled.append(dfr)
-    dfs = dfs_sampled
-
-    # TODO: figure out a more SQL-y way to do this. Ideally we wouldn't even
-    # need the dataframe split.
-    df_base = [df for df in dfs if df["optimizer"].iloc[0] == "expert"]
-    assert len(df_base) == 1
-    df_base = df_base[0]
-    for df in dfs:
-        df[REGRET] = df[COST_CUM] - df_base[COST_CUM]
-    dfcat = pd.concat(dfs).reset_index()
-
-    sns.lineplot(dfcat, ax=ax_cost, x=TIME, y="cost", hue="optimizer")
-    sns.lineplot(dfcat, ax=ax_err, x=TIME, y=ERR, hue="optimizer")
-    sns.lineplot(dfcat, ax=ax_cum, x=TIME, y=COST_CUM, hue="optimizer")
-    sns.lineplot(dfcat, ax=ax_regret, x=TIME, y=REGRET, hue="optimizer")
-    # TODO: work on legends.
-
-    if style != BAD_INIT:
-        for ax in axs_cost:
-            shade_fan(dfs[0], ax)
-            ax.legend()
-
-    fig_cost.savefig(f"{style}_cost.pdf")
-
-
-def plot_costs_v2(dfs: Sequence[pd.DataFrame], style):
-
     sns.set_style("whitegrid")
 
     fig_cost, axs = plt.subplots(1, 2, figsize=(10, 2.25), constrained_layout=True)
@@ -525,7 +484,7 @@ def main():
         episodic(dfs[0], dfs[1:])
     else:
         plot_params(dfs, style)
-        plot_costs_v2(dfs, style)
+        plot_costs(dfs, style)
         if style != FAN:
             plot_fig8(dfs, style)
 
