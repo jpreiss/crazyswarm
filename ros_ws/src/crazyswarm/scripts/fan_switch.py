9e3e7036bd0d0ce2496de9bc131f55b8e1494eda
jpreiss <jamesalanpreiss@gmail.com>
2024-07-08 23:37:07 -0700

    Serial context mgr

--- ros_ws/src/crazyswarm/scripts/fan_switch.py
+++ ros_ws/src/crazyswarm/scripts/fan_switch.py
@@ -34,26 +34,26 @@ def main():
         raise IOError("No Arduino serial device found.")
     dev = devs[0]
     print("using device " + dev)
-    arduino = serial.Serial(dev, baudrate=115200, timeout=1)
+    with serial.Serial(dev, baudrate=115200, timeout=1) as arduino:
+        # wait for arduino to be ready
+        while True:
+            line = arduino.readline()
+            # because it sends weird line endings
+            print("serial:", line)
+            if line.startswith(READY_MSG):
+                break
+        print(READY_MSG)
 
-    # wait for arduino to be ready
-    while True:
-        line = arduino.readline()
-        # because it sends weird line endings
-        if line.startswith(READY_MSG):
-            break
-    print("arduino is ready.")
+        # turn fan off initially
+        arduino.write(bytes("f0", "ascii"))
+        # turn light off initially
+        b = bytes([CHR_L, 0, 0, 0, 0])
+        arduino.write(b)
 
-    # turn fan off initially
-    arduino.write(bytes("f0", "ascii"))
-    # turn light off initially
-    b = bytes([CHR_L, 0, 0, 0, 0])
-    arduino.write(b)
-
-    rospy.init_node("fan_switcher")
-    rospy.Subscriber("fan", std_msgs.msg.Bool, fan_callback, queue_size=1)
-    rospy.Subscriber("light", std_msgs.msg.ColorRGBA, light_callback, queue_size=1)
-    rospy.spin()
+        rospy.init_node("fan_switcher")
+        rospy.Subscriber("fan", std_msgs.msg.Bool, fan_callback, queue_size=1)
+        rospy.Subscriber("light", std_msgs.msg.ColorRGBA, light_callback, queue_size=1)
+        rospy.spin()
 
 
 if __name__ == "__main__":
