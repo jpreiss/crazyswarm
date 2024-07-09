06574a2fa9b6462427391a60a11d5f0b9ac80ea2
jpreiss <jamesalanpreiss@gmail.com>
2024-07-08 18:18:20 -0700

    LINE traj can also have lower Z

--- ros_ws/src/crazyswarm/scripts/gaps.py
+++ ros_ws/src/crazyswarm/scripts/gaps.py
@@ -114,7 +114,7 @@ def rollout(cf, gaps, Z, radius, timeHelper, pub, trajmode, repeats, period, fan
     """The part of the flight where we use low-level commands."""
     radius = 0.75
     init_pos = cf.initialPosition + [0, 0, Z]
-    if trajmode != HORIZ:
+    if trajmode not in [HORIZ, LINE]:
         assert Z > radius / 2 + 0.2
     traj_major = TrigTrajectory.Cosine(amplitude=radius, period=period)
     if trajmode == CIRCLE:
